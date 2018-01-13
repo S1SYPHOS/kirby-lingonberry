b8f8b3399df3a388cdc5f54aa63451e161c8a877
S1SYPHOS <hello@twobrain.io>
2018-01-13 11:49:21 +0100

    Improving loop.php && adding inline documentation

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -1,10 +1,33 @@
 <?php
 
+/*
+
+---------------------------------------
+Home controller
+---------------------------------------
+
+This controller defines all variables used by
+the home template: Each view ('search', 'query
+variables' and 'posts') requires a given term,
+the corresponding posts and their pagination.
+
+The $perpage variable determines how many posts
+should be shown per page.
+
+The $types array consists of all files inside the
+directory 'site/snippets/home', which makes it quite
+easy to customise available query variables.
+
+*/
+
 return function($site, $pages, $page) {
 
   // Max number of posts shown per page
   $perpage  = $page->perpage()->int();
 
+  // All query variables
+  $types = dir::read(kirby()->roots()->snippets() . '/home');
+  
   // Posts
   $posts = $page->children()
                 ->visible()
@@ -68,36 +91,39 @@ return function($site, $pages, $page) {
                   ->paginate(($perpage >= 1)? $perpage : 5);
 
   return [
-    'posts' => $posts,
-    'pagination' => $posts->pagination(),
+    'params'               => params(),
+    'types'                => $types,
 
-    'tag' => $tag,
-    'postsByTag' => $postsByTag,
-    'paginationByTag' => $postsByTag->pagination(),
+    'posts'                => $posts,
+    'pagination'           => $posts->pagination(),
 
-    'category' => $category,
-    'postsByCategory' => $postsByCategory,
+    'tag'                  => $tag,
+    'postsByTag'           => $postsByTag,
+    'paginationByTag'      => $postsByTag->pagination(),
+
+    'category'             => $category,
+    'postsByCategory'      => $postsByCategory,
     'paginationByCategory' => $postsByCategory->pagination(),
 
-    'author' => $author,
-    'user' => $user,
-    'postsByAuthor' => $postsByAuthor,
-    'paginationByAuthor' => $postsByAuthor->pagination(),
+    'author'               => $author,
+    'user'                 => $user,
+    'postsByAuthor'        => $postsByAuthor,
+    'paginationByAuthor'   => $postsByAuthor->pagination(),
 
-    'year' => $year,
-    'postsByYear' => $postsByYear,
-    'paginationByYear' => $postsByYear->pagination(),
+    'year'                 => $year,
+    'postsByYear'          => $postsByYear,
+    'paginationByYear'     => $postsByYear->pagination(),
 
-    'month' => $month,
-    'postsByMonth' => $postsByMonth,
-    'paginationByMonth' => $postsByMonth->pagination(),
+    'month'                => $month,
+    'postsByMonth'         => $postsByMonth,
+    'paginationByMonth'    => $postsByMonth->pagination(),
 
-    'day' => $day,
-    'postsByDay' => $postsByDay,
-    'paginationByDay' => $postsByDay->pagination(),
+    'day'                  => $day,
+    'postsByDay'           => $postsByDay,
+    'paginationByDay'      => $postsByDay->pagination(),
 
-    'query'   => $query,
-    'results' => $results,
-    'paginationByResults' => $results->pagination()
+    'query'                => $query,
+    'results'              => $results,
+    'paginationByResults'  => $results->pagination()
   ];
 };
